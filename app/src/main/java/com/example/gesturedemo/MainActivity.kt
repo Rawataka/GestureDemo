fe072bd469de41c762e2d05df7ba3b8f02efc84c
X_Admin <1kostr1@mail.ru>
2025-12-15 23:01:43 +0700

    ScrollableModifier

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -30,6 +31,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -58,27 +60,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    PointerInputDrag(modifier)
+    ScrollableModifier(modifier)
 }
 @Composable
-fun PointerInputDrag(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-        var xOffset by remember { mutableStateOf(0f) }
-        var yOffset by remember { mutableStateOf(0f) }
-        Box(
-            Modifier
-                .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
-                .background(Color.Blue)
-                .size(100.dp)
-                .pointerInput(Unit) {
-                    detectDragGestures { _, distance ->
-                        xOffset += distance.x
-                        yOffset += distance.y
-                    }
+fun ScrollableModifier(modifier: Modifier = Modifier) {
+    var offset by remember { mutableStateOf(0f) }
+    Box(
+        modifier
+            .fillMaxSize()
+            .scrollable(
+                orientation = Orientation.Vertical,
+                state = rememberScrollableState { distance ->
+                    offset += distance
+                    distance
                 }
-        )
+            )
+    ) {
+        Box(modifier = Modifier
+            .size(90.dp)
+            .offset { IntOffset(0, offset.roundToInt()) }
+            .background(Color.Red))
     }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
